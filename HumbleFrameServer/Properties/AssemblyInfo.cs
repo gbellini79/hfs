705aa18fe906bca9ff43ec8e9a50f2dda27a26d4
Giacomo Bellini <gbellini79@users.noreply.github.com>
2024-08-03 00:30:49 +0200

    Initial conversion to .NET 8

--- HumbleFrameServer/Properties/AssemblyInfo.cs
+++ HumbleFrameServer/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("HumbleFrameServer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HumbleFrameServer")]
-[assembly: AssemblyCopyright("Copyright © Giacomo Bellini 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bdd7f013-05de-434c-875d-b58f98bc6951")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.3")]
-[assembly: AssemblyInformationalVersion("WAAAAAV")]
