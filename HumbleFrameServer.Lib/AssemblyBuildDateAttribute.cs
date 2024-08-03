84e8b3af34216d72d2c635d2fec722b21401c37e
Giacomo Bellini <gbellini79@users.noreply.github.com>
2024-08-04 00:13:28 +0200

    Automatic build date

--- HumbleFrameServer.Lib/AssemblyBuildDateAttribute.cs
+++ HumbleFrameServer.Lib/AssemblyBuildDateAttribute.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HumbleFrameServer.Lib
 {
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
-    public sealed class AssemblyBuildDateAttribute : Attribute
+    public sealed class AssemblyBuildDateAttribute(string buildDate) : Attribute
     {
-        public string BuildDate { get; set; }
+        public string BuildDate { get; set; } = buildDate;
     }
 }
--- HumbleFrameServer/Properties/AssemblyBuildDate.cs
+++
@@ -1,2 +0,0 @@
-using HumbleFrameServer.Lib;
-[assembly: AssemblyBuildDate(BuildDate = "19/10/2019 17:28:21,73")]
